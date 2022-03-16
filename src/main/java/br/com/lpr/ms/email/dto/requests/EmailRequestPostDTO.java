5994cd7d0abae6746ab2e3528f6a413f305aaea9
lucasprodrigues22 <lucasp.rodrigues22@outlook.com>
2022-03-16 07:42:33 -0300

    #ADD implementando EmailRequestPost

--- src/main/java/br/com/lpr/ms/email/dto/requests/EmailRequestPostDTO.java
+++ src/main/java/br/com/lpr/ms/email/dto/requests/EmailRequestPostDTO.java
@@ -1,5 +1,24 @@
 package br.com.lpr.ms.email.dto.requests;
 
-public class EmailRequestPostDTO {
+import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
+public class EmailRequestPostDTO {
+	
+	@NotBlank
+	private UUID userId;
+	
+	@NotBlank
+	private String from;
+	
+	@NotBlank
+	private String to;
+	
+	@NotBlank
+	private String subject;
+	
+	@NotBlank
+	private String text;
+	
 }
