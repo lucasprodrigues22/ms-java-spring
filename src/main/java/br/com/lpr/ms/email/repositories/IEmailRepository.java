d48c7fead35d0a38b4ef9099ba04e64a9f7a7328
lucasprodrigues22 <lucasp.rodrigues22@outlook.com>
2022-03-19 17:39:50 -0300

    Serviço de email: #CHANGE IEmailRepository #ADD configuração .properties

--- src/main/java/br/com/lpr/ms/email/repositories/IEmailRepository.java
+++ src/main/java/br/com/lpr/ms/email/repositories/IEmailRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.lpr.ms.email.entities.Email;
 
-public interface EmailRepository extends JpaRepository<Email, UUID> {
+public interface IEmailRepository extends JpaRepository<Email, UUID> {
 
 }
--- src/main/java/br/com/lpr/ms/email/services/EmailService.java
+++ src/main/java/br/com/lpr/ms/email/services/EmailService.java
@@ -3,14 +3,15 @@ package br.com.lpr.ms.email.services;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.lpr.ms.email.entities.Email;
-import br.com.lpr.ms.email.repositories.EmailRepository;
+import br.com.lpr.ms.email.repositories.IEmailRepository;
 
 public class EmailService {
 
 	@Autowired
-	private EmailRepository repository;
+	private IEmailRepository repository;
 	
 	public void send(Email email) {
+		
 	}
 
 	
